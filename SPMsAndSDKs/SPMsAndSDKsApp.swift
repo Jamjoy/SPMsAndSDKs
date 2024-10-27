6fcc0bffa74da635956b7c537693f087872e4ab4
Joyanta Mondal <joyantamondal156@gmail.com>
2024-10-27 13:16:25 +0530

    Initial Commit

---
+++ SPMsAndSDKs/SPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMsAndSDKsApp.swift
+//  SPMsAndSDKs
+//
+//  Created by Joyanta Mondal on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
